0ee41a913b21607b82c35c5022694a5b0c25b881
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2022-10-29 15:31:03 +0800

    Update InjectingCode.js

--- scripts/InjectingCode.js
+++ scripts/InjectingCode.js
@@ -35,7 +35,7 @@ hexo.extend.injector.register('body_end', `<footer style="background: var(--boar
         <a href="https://icp.gov.moe/?keyword=20220247" target="_blank">萌ICP备20220247号</a>
         <br/>
         
-        <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" target="_blank"><img src="http://hotpic.myzwq.com/i/2022/10/29/635cd621ca652.png"></a>
+        <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral" target="_blank"><img src="https://pic1.imgdb.cn/item/635cd73016f2c2beb19b7fec.png"></a>
         <br/>
         <a id="RunTime" style=""></a>
 </div>
